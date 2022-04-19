d11ad6c90a7c5e644decc4f1cefc5d2dacb740d0
Stavros <snikoletopoulos@hotmail.com>
2022-04-20 02:27:06 +0300

    Use new model inside products controller

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "express";
 
-const products: any[] = [];
+import Product from "../models/product";
 
 export const addProduct: RequestHandler = (req, res) => {
 	res.render("add-product", {
@@ -13,17 +13,21 @@ export const addProduct: RequestHandler = (req, res) => {
 };
 
 export const postAddProduct: RequestHandler = (req, res) => {
-	products.push({ title: req.body.title });
+	const product = new Product(req.body.title);
+	product.save();
+
 	res.redirect("/");
 };
 
 export const getProducts: RequestHandler = (req, res) => {
-	res.render("shop", {
-		pageTitle: "Shop",
-		path: "/",
-		productCSS: true,
-		activeShop: true,
-		hasProducts: products.length > 0,
-		prods: products,
+	Product.fetchAll(products => {
+		res.render("shop", {
+			pageTitle: "Shop",
+			path: "/",
+			productCSS: true,
+			activeShop: true,
+			hasProducts: products.length > 0,
+			prods: products,
+		});
 	});
 };
