92baecfd715c4f966947eed49348fd7c16a2288c
Stavros <snikoletopoulos@hotmail.com>
2022-04-24 20:31:36 +0300

    Fix price type

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -15,7 +15,7 @@ export const addProduct: RequestHandler = (req, res) => {
 export const postAddProduct: RequestHandler = (req, res) => {
 	const { title, imageUrl, description, price } = req.body;
 
-	const product = new Product(title, imageUrl, description, price);
+	const product = new Product(title, imageUrl, description, +price);
 	product.save();
 
 	res.redirect("/");
