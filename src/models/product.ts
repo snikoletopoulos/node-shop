925a791ee6a4b119eb77d203c6fc9e5d54592c89
Stavros <snikoletopoulos@hotmail.com>
2022-05-01 00:05:10 +0300

    Create interface for typescript sequelize support

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,28 +1,46 @@
-import Sequelize from "sequelize";
+import {
+	Model,
+	DataTypes,
+	CreationOptional,
+	InferAttributes,
+	InferCreationAttributes,
+} from "sequelize";
 
 import sequelize from "../helpers/db.helpers";
 
-const Product = sequelize.define("product", {
+interface ProductModel
+	extends Model<
+		InferAttributes<ProductModel>,
+		InferCreationAttributes<ProductModel>
+	> {
+	id: CreationOptional<number>;
+	title: string;
+	price: number;
+	description: string;
+	imageUrl: string;
+}
+
+const Product = sequelize.define<ProductModel>("product", {
 	id: {
-		type: Sequelize.INTEGER,
+		type: DataTypes.INTEGER,
 		autoIncrement: true,
 		allowNull: false,
 		primaryKey: true,
 	},
 	title: {
-		type: Sequelize.STRING,
+		type: DataTypes.STRING,
 		allowNull: false,
 	},
 	price: {
-		type: Sequelize.DOUBLE,
+		type: DataTypes.DOUBLE,
 		allowNull: false,
 	},
 	imageUrl: {
-		type: Sequelize.STRING,
+		type: DataTypes.STRING,
 		allowNull: false,
 	},
 	description: {
-		type: Sequelize.TEXT,
+		type: DataTypes.TEXT,
 		allowNull: false,
 	},
 });
