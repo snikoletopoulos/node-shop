bb60d9d3980e25008b7bb0011754f01e140b1d6d
Stavros <snikoletopoulos@hotmail.com>
2022-05-01 19:38:34 +0300

    Register the user in the request object

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import adminRouter from "./routes/admin.routes";
 import shopRouter from "./routes/shop.routes";
 import { get404 } from "./controllers/error.controllers";
 import sequelize from "./helpers/db.helpers";
+import Product from "./models/product";
+import User from "./models/user";
 
 const app = express();
 
@@ -15,6 +17,22 @@ app.set("views", "src/views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(async (req, res, next) => {
+	try {
+		const user = await User.findByPk(1);
+
+		if (!user) {
+			throw new Error("User not found");
+		}
+
+		req.user = user;
+
+		next();
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
