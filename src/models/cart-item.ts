4821234f78f8ccb79d80a600d390d8d784b4abc1
Stavros <snikoletopoulos@hotmail.com>
2022-05-08 17:20:26 +0300

    Create CartItem model with Sequelize

--- src/models/cart-item.ts
+++ src/models/cart-item.ts
@@ -1,12 +1,43 @@
-import Product from "./product";
+import {
+	DataTypes,
+	Model,
+	InferAttributes,
+	InferCreationAttributes,
+	CreationOptional,
+	ForeignKey,
+} from "sequelize/types";
 
-class CartItem {
-	constructor(public id: number, public quantity: number) {}
+import sequelize from "../helpers/db.helpers";
 
-	async getProduct() {
-		const product = await Product.findByPk(this.id);
-		return product;
-	}
+interface CartItemModel
+	extends Model<
+		InferAttributes<CartItemModel>,
+		InferCreationAttributes<CartItemModel>
+	> {
+	id: CreationOptional<number>;
+	quantity: number;
+	productId: ForeignKey<number>;
 }
+
+const CartItem = sequelize.define<CartItemModel>("cart-item", {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+	quantity: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	productId: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: {
+			model: "product",
+			key: "id",
+		},
+	},
+});
 
 export default CartItem;
