de589cb1bac91574a410242880e8aca89d68cf61
Stavros <snikoletopoulos@hotmail.com>
2022-05-08 17:19:41 +0300

    Create Cart model with Sequelize

--- src/models/cart.ts
+++ src/models/cart.ts
@@ -1,136 +1,33 @@
-import path from "path";
-import fs from "fs/promises";
+import {
+	DataTypes,
+	Model,
+	InferAttributes,
+	InferCreationAttributes,
+	CreationOptional,
+} from "sequelize/types";
 
-import { baseFilePath } from "../constants/files.constants";
-import CartItem from "./cart-item";
+import sequelize from "../helpers/db.helpers";
+import Product from "./product";
+import User from "./user";
 
-class Cart {
-	static products: CartItem[] = [];
-	static totalPrice = 0;
-
-	static readonly filePath = path.join(baseFilePath, "cart.json");
-
-	static getObject() {
-		return {
-			products: Cart.products,
-			totalPrice: Cart.totalPrice,
-		};
-	}
-
-	static async addProduct(id: number) {
-		return Cart.updateCart(async () => {
-			const existingProductIndex = Cart.products.findIndex(
-				product => product.id === id
-			);
-
-			let updatedCartItem: CartItem;
-			if (existingProductIndex !== -1) {
-				updatedCartItem = Cart.products[existingProductIndex];
-				updatedCartItem.quantity += 1;
-			} else {
-				updatedCartItem = new CartItem(id, 1);
-				Cart.products.push(updatedCartItem);
-			}
-
-			const product = await updatedCartItem.getProduct();
-
-			if (!product) {
-				throw new Error("Product(s) not found");
-			}
-
-			Cart.totalPrice += product.price;
-		});
-	}
-
-	static async removeItem(id: number) {
-		await Cart.updateCart(async () => {
-			const cartItem = Cart.products.find(product => product.id === id);
-
-			if (!cartItem) {
-				return;
-			}
-
-			const product = await cartItem.getProduct();
-
-			if (!product) {
-				throw new Error("Product(s) not found");
-			}
-
-			if (cartItem.quantity > 1) {
-				cartItem.quantity -= 1;
-				Cart.totalPrice -= product.price;
-			} else {
-				Cart.deleteProduct(id);
-			}
-		});
-	}
-
-	static deleteProduct(id: number) {
-		return Cart.updateCart(async () => {
-			const cartItem = Cart.products.find(product => product.id === id);
-
-			if (!cartItem) {
-				return;
-			}
-
-			const updatedCart = Cart.products.filter(product => product.id !== id);
-			const product = await cartItem.getProduct();
-
-			if (!product) {
-				throw new Error("Product(s) not found");
-			}
-
-			Cart.totalPrice -= cartItem.quantity * product.price;
-			Cart.products = updatedCart;
-		});
-	}
-
-	static async getCart() {
-		try {
-			const fileContent = await fs.readFile(Cart.filePath);
-			const cart = JSON.parse(fileContent.toString());
-			Cart.products = cart.products.map(
-				(cartItemData: CartItem) =>
-					new CartItem(cartItemData.id, cartItemData.quantity)
-			);
-			Cart.totalPrice = +cart.totalPrice;
-
-			return cart;
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
-	static async getProducts() {
-		await Cart.getCart();
-		const products = await Promise.all(
-			Cart.products.map(async cartItem => {
-				const product = await cartItem.getProduct();
-				return {
-					...product,
-					quantity: cartItem.quantity,
-				};
-			})
-		);
-		return products;
-	}
-
-	static updateFileCart() {
-		fs.writeFile(Cart.filePath, JSON.stringify(Cart.getObject())).catch(
-			error => {
-				if (error) {
-					console.log(error);
-				}
-			}
-		);
-	}
-
-	static async updateCart<T>(callback: () => Promise<T>) {
-		await Cart.getCart();
-		const response = await callback();
-		Cart.updateFileCart();
-		return response;
-	}
+interface CartModel
+	extends Model<
+		InferAttributes<CartModel>,
+		InferCreationAttributes<CartModel>
+	> {
+	id: CreationOptional<number>;
 }
+
+const Cart = sequelize.define<CartModel>("cart", {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+});
+
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: "cart-item" });
 
 export default Cart;
