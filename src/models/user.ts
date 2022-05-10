fd0fb18a00a1e341c0a193257832c60228e3fe2a
Stavros <snikoletopoulos@hotmail.com>
2022-05-11 02:38:06 +0300

    Refactor User model to use mongoose

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,46 +1,32 @@
-import {
-	DataTypes,
-	Model,
-	InferAttributes,
-	InferCreationAttributes,
-	CreationOptional,
-} from "sequelize";
+import { Schema, model, Types } from "mongoose";
 
-import sequelize from "../helpers/db.helpers";
-import Product from "./product";
-import Cart from "./cart";
-import Order from "./order";
-
-export interface UserModel
-	extends Model<
-		InferAttributes<UserModel>,
-		InferCreationAttributes<UserModel>
-	> {
-	id: CreationOptional<number>;
+export interface IUser {
 	name: string;
 	email: string;
+	cart: Types.Array<{ productId: Types.ObjectId; quantity: number }>;
 }
 
-const User = sequelize.define<UserModel>("user", {
-	id: {
-		type: DataTypes.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
-	},
+const userSchema = new Schema<IUser>({
 	name: {
-		type: DataTypes.STRING,
-		allowNull: false,
+		type: String,
+		required: true,
 	},
 	email: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		unique: true,
+		type: String,
+		required: true,
+	},
+	cart: {
+		items: [
+			{
+				productId: {
+					type: Schema.Types.ObjectId,
+					ref: "Product",
+					required: true,
+				},
+				quantity: { type: Number, required: true },
+			},
+		],
 	},
 });
 
-User.hasMany(Product);
-User.hasOne(Cart);
-User.hasMany(Order);
-
-export default User;
+export default model<IUser>("User", userSchema);
